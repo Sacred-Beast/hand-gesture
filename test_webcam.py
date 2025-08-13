c1827bda73c595c22d902eba52c0fd6b0907b0fe
Sacred-Beast <jayofficial158@gmail.com>
2025-08-13 10:35:39 +0530

    commit test_webcam

---
+++ test_webcam.py
@@ -0,0 +1,16 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Could not open webcam")
+else:
+    ret, frame = cap.read()
+    if ret:
+        cv2.imshow("Webcam Test", frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        print("Error: Could not read frame")
+
+cap.release()
